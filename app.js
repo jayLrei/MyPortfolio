cde1565314c9579fcda76b48fbef033255b3fcf1
jayLrei <thfpdhk1@gmail.com>
2023-03-09 23:05:25 +0900

    feat : work page 70% complete

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ navLinks.forEach( link => {
 //캐러셀
 
 const carousel = document.querySelector('.carousel-container');
-const wrapper = document.querySelector('.carousel-wrapper');
+const wrapper = carousel.querySelector('.carousel-wrapper');
 const images = wrapper.querySelectorAll('img');
 const imageWidth = images[0].offsetWidth;
 let position = -imageWidth;
@@ -45,37 +45,6 @@ let slideInterval = setInterval(moveSlides, 3000);
 
 
 //work페이지 캐러셀
-const slides = document.querySelector(".slides");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-let counter = 1;
-const size = slides.clientWidth / 5;
 
-slides.style.transform = `translateX(${-size * counter}px)`;
 
-nextBtn.addEventListener("click", () => {
-  if (counter >= slides.children.length - 1) return;
-  slides.style.transition = "transform 0.5s ease-in-out";
-  counter++;
-  slides.style.transform = `translateX(${-size * counter}px)`;
-});
 
-prevBtn.addEventListener("click", () => {
-  if (counter <= 0) return;
-  slides.style.transition = "transform 0.5s ease-in-out";
-  counter--;
-  slides.style.transform = `translateX(${-size * counter}px)`;
-});
-
-slides.addEventListener("transitionend", () => {
-  if (slides.children[counter].id === "lastClone") {
-    slides.style.transition = "none";
-    counter = slides.children.length - 2;
-    slides.style.transform = `translateX(${-size * counter}px)`;
-  }
-  if (slides.children[counter].id === "firstClone") {
-    slides.style.transition = "none";
-    counter = slides.children.length - counter;
-    slides.style.transform = `translateX(${-size * counter}px)`;
-  }
-});
